843fed4e580f0c932af54639c7b2857ec2dcd7a9
limhasic <oilbread@naver.com>
2025-06-17 14:11:44 +0900

    delete_port

--- 20250617_gradio_yolo.py
+++ 20250617_gradio_yolo.py
@@ -306,6 +306,6 @@ if __name__ == "__main__":
     demo.launch(
         share=True,
         server_name="0.0.0.0",
-        server_port=7860,
+        #server_port=7860,
         show_error=True
     )
